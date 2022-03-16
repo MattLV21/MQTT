0b6af57befaa32a6bf6dbd66aa86e01158afd31b
MattLV21 <Mattias.lange00@gmail.com>
2022-03-16 10:55:20 +0100

    DDU client
    dea

---
+++ Client/sketch.js
@@ -0,0 +1,147 @@
+let client;
+let id = "200d00";
+
+function setup(){
+  mqttInit()
+
+  client.subscribe(id);
+  //når vi modtager beskeder fra MQTT serveren kaldes denne funktion
+  client.on('message', (topic, message) => {
+    console.log('Received Message: ' + message.toString() + '\nOn topic: ' + topic);
+    resiveChallenge(message);
+  });
+}
+
+function resiveChallenge(challenge) {
+  select("#Challenge_Text").html(challenge.toString());
+}
+
+function toGet() {
+  //location.replace("resiveChallenge.html");
+  client.publish("giveChallenge", id);
+}
+function toMake() {
+  location.replace("makeChallenge.html");
+}
+function toHome() {
+  location.replace("home.html");
+}
+
+function Search() {
+  var link = document.getElementById("link").value
+  client.publish('Konge_spil', link)
+  console.log("send");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const mqttInit = () => {
+  //opret et id med en random talkode og sæt gem servernavnet i en variabel
+  const clientId = 'mqttjs_' + Math.random().toString(16).substr(2, 8)
+  const host = 'wss://mqtt.nextservices.dk'
+
+  //opret et objekt med de oplysninger der skal bruges til at forbinde til serveren
+  const options = {
+    keepalive: 300,
+    clientId: clientId,
+    protocolId: 'MQTT',
+    protocolVersion: 4,
+    clean: true,
+    reconnectPeriod: 1000,
+    connectTimeout: 30 * 1000,
+    will: {
+      topic: 'WillMsg',
+      payload: 'Connection Closed abnormally..!',
+      qos: 0,
+      retain: false
+    },
+    rejectUnauthorized: false
+  }
+
+  console.log('connecting mqtt client')
+
+  //forsøg at oprette forbindelse 
+  client = mqtt.connect(host, options)
+
+  //hvis der sker en fejl kaldes denne funktion
+  client.on('error', (err) => {
+    console.log('Connection error: ', err)
+    client.end()
+  })
+
+  //og hvis forbindelsen mistes kaldes denne funktion
+  client.on('reconnect', () => {
+    console.log('Reconnecting...')
+  })
+
+  //hvis forbindelsen lykkes kaldes denne funktion
+  client.on('connect', () => {
+    console.log('Client connected:' + clientId)
+  })
+
+  //når forbindelsen lukkes kaldes denne funktion
+  client.on('close', () => {
+    console.log(clientId + ' disconnected')
+  })
+} 
+
+function showChart(){
+  //opret chart 
+  chart = new Chart(select('#chartCanvas').elt, {
+      type: 'bar',
+      data: {
+        labels: ['hej', 'med', 'dig'],
+        datasets: [{
+            label: 'Resultat',
+            data: [33,10,23],
+              backgroundColor: ['lightred', 'lightgreen', 'lightblue'],
+              borderWidth: 3
+          }]
+      },
+  });
+}
